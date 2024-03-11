e7e69776e3bd78c241b9496154700100486eb585
Keon Kim <fnc6488@gmail.com>
2024-03-11 18:27:49 +0900

    HTTP Request의 요청 파일을 HTTP Response 에 담아 보내는 기능 구현
    디렉토리를 요청하면 해당 디렉토리의 `index.html` 파일을 반환하도록 구현
    HTTP Response의 content-type을 `text`, `image` 등으로 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,15 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+
 public class RequestHandler implements Runnable {
+
+    private static final String STATIC_PATH = "src/main/resources/static";
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -32,18 +32,50 @@ public class RequestHandler implements Runnable {
 
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
-            response200Header(dos, body.length);
+
+            String filePath;
+            if (startLine[1].contains(".")) {
+                filePath = STATIC_PATH + startLine[1];
+            } else {
+                filePath = STATIC_PATH + startLine[1] + "/index.html";
+            }
+
+            File file = new File(filePath);
+            byte[] body = new byte[(int) file.length()];
+            try (FileInputStream fis = new FileInputStream(file)) {
+                fis.read(body);
+            }
+
+            String[] splitFilePath = filePath.split("/");
+            String fileName = splitFilePath[splitFilePath.length - 1];
+            String contentType = getContentType(fileName.split("\\.")[1]);
+
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private String getContentType(String type) {
+        String contentType;
+        if (type.equals("html") || type.equals("css")) {
+            contentType = "text/" + type;
+        } else if (type.equals("svg")) {
+            contentType = "image/svg+xml";
+        } else if (type.equals("ico")) {
+            contentType = "image/x-icon";
+        } else {
+            throw new IllegalArgumentException("");
+        }
+
+        return contentType;
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
