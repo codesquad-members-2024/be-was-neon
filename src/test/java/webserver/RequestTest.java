f8edc191ca6bcfb08a028694ed56032a7eac8ebb
Miensoap <miensop51@gmail.com>
2024-03-12 15:42:04 +0900

    Request 테스트 코드 작성 , 파일 경로 상대경로로 수정

---
+++ src/test/java/webserver/RequestTest.java
@@ -0,0 +1,38 @@
+package webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class RequestTest {
+
+    @ParameterizedTest
+    @CsvSource({"GET /index.html , getFile /index.html" , "GET /, getFile //index.html" , "GET /registration , getFile /registration/index.html"})
+    @DisplayName("getFile 요청에 대해 알맞은 Request 객체가 반환되고 , url 이 경로라면 해당 경로의 index.html 을 요청한 것으로 간주한다")
+    void getFileRequest(String url , String log) throws IOException {
+        Request request = makeRequest(url);
+        assertThat(request.getLog()).isEqualTo(log);
+    }
+
+    @Test
+    @DisplayName("createUser 요청에 대해 매개변수가 알맞게 파싱되고 , Request 객체가 반환되어야 한다")
+    void createUser() throws IOException {
+        String url = "GET /create?userId=test&password=test&name=test&email=test%40naver.com";
+        Request request = makeRequest(url);
+        assertThat(request.getLog()).isEqualTo("createUser /create?userId=test&password=test&name=test&email=test%40naver.com");
+        assertThat(request.getParams().get(1)).isEqualTo("test");
+
+    }
+
+    private static Request makeRequest(String url) throws IOException {
+        InputStream inputStream = new ByteArrayInputStream(url.getBytes());
+        return Request.makeRequest(inputStream);
+    }
+}
