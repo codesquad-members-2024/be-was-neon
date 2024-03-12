adfc0f197469c892f47978169fe2dc659a83a0a4
Gyeongtaek <ogt124321@gmail.com>
2024-03-12 14:03:34 +0900

    JDK의 nio를 사용하지 않기 Java io만을 이용하도록 코드를 수정하고 리팩토링 해주었습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,15 +2,13 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
+    private static final String RELATIVE_PATH = "./src/main/resources/static/";
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -21,20 +19,20 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
             String fileName = getFileName(line);
-            // Request header 의 로그를 콘솔에 출력해 줍니다.
+            DataOutputStream dos = new DataOutputStream(out);
             logger.debug(line);
-            while(!line.isEmpty()){
+            while (line != null && !line.isEmpty()) {
+                // Request header 의 로그를 콘솔에 출력해 줍니다.
                 line = br.readLine();
                 logger.debug(line);
             }
-            // html 파일을 모두 읽어와 Bytes 로 변환해준다.
-            byte[] file = Files.readAllBytes(new File("./src/main/resources/static/" + fileName).toPath());
-            DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, file.length);
-            responseBody(dos, file);
+            if (fileName.endsWith(".html")) {
+                // html 파일을 읽어와 Bytes 로 변환한 후에 response 를 내보낸다.
+                respondHtmlFile(dos, RELATIVE_PATH + fileName);
+            }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -62,5 +60,29 @@ public class RequestHandler implements Runnable {
         String[] getFile = firstLine.split(" ");
         return getFile[1];
     }
+    private void respondHtmlFile (DataOutputStream dos, String fileName){
+        try {
+            // 파일에서 바이트 단위로 읽기 위한 스트림을 제공합니다.
+            FileInputStream fis = new FileInputStream(new File(fileName));
+            //FileInputStream 의 성능을 개선하기 위한 보조 스트림으로, 버퍼링된 입력 스트림입니다.
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            //바이트 배열에 데이터를 쓰기 위한 스트림입니다.
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            //read()는 파일의 끝에 도달하면 -1을 반환합니다.
+            while ((bytesRead = bis.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesRead);
+            }
+            // 파일 데이터를 fileBytes 배열로 읽어옵니다.
+            byte[] fileBytes = baos.toByteArray();
+            // response 를 http 로 보냅니다.
+            response200Header(dos, fileBytes.length);
+            responseBody(dos, fileBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
