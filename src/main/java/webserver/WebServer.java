f3394219fc672fe30110ec12a64bb672a9ef519b
choejieun <wldms5221@icloud.com>
2024-03-13 01:38:35 +0900

    Concurrent패키지 10개의 스레드 풀에 작업을 할당하고 클라이언트의 요청을 처리할 수 있도록 함

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int THREAD_POOL_SIZE = 10;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -18,16 +21,20 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        //스레드 풀 생성
+        ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
-            Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+            while (true) {
+                // 클라이언트가 연결될 때마다 스레드 풀에서 작업 할당
+                Socket connection = listenSocket.accept();
+                threadPool.execute(new RequestHandler(connection));
             }
+        } finally {
+            threadPool.shutdown();
         }
     }
 }
