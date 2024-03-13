2183c9d2058f640685df41d1d32738b3a65b6ff8
Yeriimii <seojs0511@naver.com>
2024-03-14 03:31:01 +0900

    feat: 회원가입 기능 구현
    - form을 통해 userId, name, password, email을 입력하면 데이터베이스에 저장된다

---
+++ src/main/java/web/MemberSave.java
@@ -0,0 +1,24 @@
+package web;
+
+import db.Database;
+import http.HttpRequest;
+import http.HttpResponse;
+import model.User;
+
+public class MemberSave extends HtmlProcessor {
+
+    @Override
+    public void process(HttpRequest request, HttpResponse response) {
+        if (request.getParameter("id").isEmpty()) {
+            super.process(request, response);
+            return;
+        }
+            String id = request.getParameter("id");
+            String username = request.getParameter("username");
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+
+            Database.addUser(new User(id, password, username, email));
+            super.process(request, response);
+    }
+}
