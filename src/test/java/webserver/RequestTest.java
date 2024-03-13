139fd24784a76313819182aeb79cda6c5ff06fe6
Miensoap <miensop51@gmail.com>
2024-03-13 10:52:26 +0900

    Request 멤버 중 필수인 method , url 은 생성자로 옮김 , 로그 변경에 따라 테스트 코드 수정

--- src/test/java/webserver/RequestTest.java
+++ src/test/java/webserver/RequestTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class RequestTest {
 
     @ParameterizedTest
-    @CsvSource({"GET /index.html , getFile /index.html"})
+    @CsvSource({"GET /index.html , GETFILE  :  /index.html"})
     @DisplayName("getFile 요청에 대해 알맞은 Request 객체가 반환된다")
     void getFileRequest(String url , String log) throws IOException {
         Request request = makeRequest(url);
@@ -22,7 +22,7 @@ public class RequestTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"GET /, getFile //index.html" , "GET /registration , getFile /registration/index.html"})
+    @CsvSource({"GET /, GETFILE  :  //index.html" , "GET /registration , GETFILE  :  /registration/index.html"})
     @DisplayName("url 이 경로라면 해당 경로의 index.html 을 요청한 것으로 간주한다")
     void getPathRequest(String url , String log) throws IOException {
         Request request = makeRequest(url);
@@ -34,7 +34,7 @@ public class RequestTest {
     void createUserRequest() throws IOException {
         String url = "GET /create?userId=test&password=test&name=test&email=test%40naver.com";
         Request request = makeRequest(url);
-        assertThat(request.getLog()).isEqualTo("createUser /create?userId=test&password=test&name=test&email=test%40naver.com");
+        assertThat(request.getLog()).isEqualTo("CREATEUSER  :  /create?userId=test&password=test&name=test&email=test%40naver.com");
         assertThat(request.getParams().get(1)).isEqualTo("test");
     }
 
