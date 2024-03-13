cc7e7d1caa5d8ab1d02511b9480e179f766bce39
Yeriimii <seojs0511@naver.com>
2024-03-14 03:27:28 +0900

    feat: Database 기능 업데이트
    - 유저 등록 로거 추가

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -5,12 +5,16 @@ import model.User;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Database {
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
     private static final Map<String, User> users = new HashMap<>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
+        logger.debug("[Database] Success Add User !");
     }
 
     public static User findUserById(String userId) {
