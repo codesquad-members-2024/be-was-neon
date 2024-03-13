4d3996e2d6020615dd45fca86eb77506c5265603
Keon Kim <fnc6488@gmail.com>
2024-03-13 11:47:29 +0900

    `HttpResponse` 클래스 리팩토링
    `HttpResponse`는 file 객체를 입력받아 header와 body를 저장
    `HttpResponse`를 전송하는 부분은 `RequestHandler`가 담당하도록 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,15 +7,18 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
 public class RequestHandler implements Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private final Socket connection;
+    private final RequestMapper requestMapper;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+        requestMapper = new RequestMapper();
     }
 
     public void run() {
@@ -24,21 +27,35 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             HttpRequest httpRequest = HttpRequest.from(in);
-            httpRequest.read();
-
+            httpRequest.log();
             String requestTarget = httpRequest.getRequestTarget();
 
-            HttpResponse httpResponse = HttpResponse.from(out, requestTarget);
-            httpResponse.send();
-
+            HttpResponse httpResponse = requestMapper.getHttpResponse(requestTarget);
+            send(out, httpResponse);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
+    private void send(OutputStream out, HttpResponse httpResponse) {
+        DataOutputStream dos = new DataOutputStream(out);
+        try {
+            writeHeader(httpResponse.getHeader(), dos);
+            writeBody(httpResponse.getBody(), dos);
+            dos.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private void writeBody(byte[] body, DataOutputStream dos) throws IOException {
+        dos.write(body, 0, body.length);
+    }
 
-
-
+    private static void writeHeader(List<String> httpResponse, DataOutputStream dos) throws IOException {
+        for (String line : httpResponse) {
+            dos.writeBytes(line);
+        }
+    }
 
 }
--- src/main/java/webserver/httpMessage/HttpResponse.java
+++ src/main/java/webserver/httpMessage/HttpResponse.java
@@ -2,95 +2,54 @@ package webserver.httpMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.TypeMapper;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpResponse {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
-    private static final String STATIC_PATH = "src/main/resources/static";
-    private static final String INDEX_HTML = "/index.html";
 
-    private final DataOutputStream dos;
-    private final String filePath;
+    private final List<String> header;
+    private final byte[] body;
 
-    private HttpResponse(DataOutputStream dos, String requestTarget) {
-        this.dos = dos;
-        filePath = getFilePath(requestTarget);
+    private HttpResponse(File file) {
+        header = getHeader(TypeMapper.getContentType(file), (int) file.length());
+        body = readFile(file);
     }
 
-    public static HttpResponse from(OutputStream out, String requestTarget) {
-        DataOutputStream dos = new DataOutputStream(out);
-        return new HttpResponse(dos, requestTarget);
+    public static HttpResponse from(File file) {
+        return new HttpResponse(file);
     }
 
-    private byte[] getBody(String filePath) throws IOException {
-        File file = new File(filePath);
+    private byte[] readFile(File file) {
         byte[] body = new byte[(int) file.length()];
         try (FileInputStream fis = new FileInputStream(file)) {
             fis.read(body);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         return body;
     }
 
-    private String getFilePath(String requestTarget) {
-        if (isDirectory(requestTarget)) {
-            return STATIC_PATH + requestTarget + INDEX_HTML;
-        } else {
-            return STATIC_PATH + requestTarget;
-        }
+    public List<String> getHeader() {
+        return header;
     }
 
-    private static boolean isDirectory(String requestTarget) {
-        return !requestTarget.contains(".");
+    public byte[] getBody() {
+        return body;
     }
 
-    public void send() throws IOException {
-        byte[] body = getBody(filePath);
+    private List<String> getHeader(String contentType, int bodyLength) {
+        List<String> header = new ArrayList<>();
 
-        response200Header(body.length);
-        responseBody(body);
-    }
+        header.add("HTTP/1.1 200 OK \r\n");
+        header.add("Content-Type: " + contentType + ";charset=utf-8\r\n");
+        header.add("Content-Length: " + bodyLength + "\r\n");
+        header.add("\r\n");
 
-    private String getFileName(String filePath) {
-        String[] splitFilePath = filePath.split("/");
-        return splitFilePath[splitFilePath.length - 1];
-    }
-
-    private void response200Header(int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + getContentType() + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private String getContentType() {
-        String fileName = getFileName(filePath);
-        String extension = getExtension(fileName);
-
-        return switch (extension) {
-            case "html", "css" -> "text/" + extension;
-            case "svg" -> "image/svg+xml";
-            case "ico" -> "image/x-icon";
-            default -> throw new IllegalArgumentException("");
-        };
-    }
-
-    private static String getExtension(String fileName) {
-        String[] split = fileName.split("\\.");
-        return split[split.length - 1];
+        return header;
     }
 }
