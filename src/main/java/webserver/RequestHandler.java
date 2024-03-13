8c03f2f03eacb25ec6a74f10975c9063a6990025
Yeriimii <seojs0511@naver.com>
2024-03-14 00:01:10 +0900

    refactor: RequestHandler 리팩토링
    - HttpRequest와 HttpResponse를 별도의 Processor 객체가 처리하도록 위임
    - Processor 객체를 찾지 못하면 Http 404 Status를 반환

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,113 +1,56 @@
 package webserver;
 
-import static utils.RequestHeaderParser.*;
+import static http.HttpStatus.*;
+import static utils.HttpRequestConverter.*;
+import static utils.HttpResponseConverter.*;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.Socket;
 
+import http.HttpResponse;
+import http.HttpRequest;
+import web.StaticMapper;
+import web.HttpProcessor;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String RESOURCE_PATH = "./src/main/resources/static";
-    private static final String HTML_EXTENSION = ".html";
-    private static final String ROOT = "/";
-    private static final String CSS_EXTENSION = ".css";
-    private static final String ICO_EXTENSION = ".ico";
-    private static final String SVG_EXTENSION = ".svg";
-    private static final String BASE_NAME = "index";
-    private static final int BUFFER_SIZE = 1024;
-
-    private Socket connection;
+    private final Socket connection;
+    private final HttpRequest request;
+    private HttpResponse response;
+    private final Runnable responseEmpty = () -> {
+        response.setHttpVersion("HTTP/1.1");
+        response.setStatusCode(STATUS_NOT_FOUND);
+        response.setContentType("text/plain");
+        response.setCharset("utf-8");
+        response.setContentLength(0);
+        response.setMessageBody("not found");
+        response.flush();
+    };
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+        request = convertToHttpRequest(connection);
+        response = convertToHttpResponse(connection);
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            DataOutputStream dos = new DataOutputStream(out);
+        // HttpRequest를 처리할 Processor 찾기
+        Optional<HttpProcessor> optionalProcessor = findProcessor(request.getRequestURI());
 
-            //TODO: 통째로 받아서 convert 하기
-            String line = reader.readLine();
-            while (line != null && !line.isEmpty()) {
-                String request = requestParse(line);
-
-                urlMapper(request, dos);
-
-                line = reader.readLine();
-            }
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+        // Processor가 존재하면 로직 실행, 없으면 404 status 반환
+        optionalProcessor.ifPresentOrElse(this::handle, responseEmpty);
     }
 
-    private void urlMapper(String request, DataOutputStream dos) {
-        if (request.equals(ROOT)) {
-            responseHtml(dos, RESOURCE_PATH + request + BASE_NAME + HTML_EXTENSION);
-            return;
-        }
-        if (request.endsWith(HTML_EXTENSION)) {
-            responseHtml(dos, RESOURCE_PATH + request);
-            return;
-        }
-        if (request.endsWith(CSS_EXTENSION)) {
-            return;
-        }
-        if (request.endsWith(SVG_EXTENSION)) {
-            return;
-        }
-        if (request.endsWith(ICO_EXTENSION)) {
-            return;
-        }
-        if (!request.isEmpty()) {
-            responseHtml(dos, RESOURCE_PATH + request + ROOT + BASE_NAME + HTML_EXTENSION);
-        }
+    public void handle(HttpProcessor processor) {
+        logger.debug("[REQUEST HANDLER] success service");
+        processor.service(request, response);
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseHtml(DataOutputStream dos, String path) {
-        File file = new File(path);
-        try (FileInputStream fis = new FileInputStream(file)) { // FileInputStream을 사용하여 파일 열기
-            byte[] byteArray = new byte[(int) file.length()]; // 파일의 크기만큼의 바이트 배열 생성
-
-            fis.read(byteArray); // 파일 내용을 바이트 배열에 읽어들임
-
-            response200Header(dos, byteArray.length);
-            responseBody(dos, byteArray);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public Optional<HttpProcessor> findProcessor(String uri) {
+        return StaticMapper.getInstance().getProcessor(uri);
     }
 }
