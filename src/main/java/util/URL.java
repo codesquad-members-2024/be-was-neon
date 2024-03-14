0a935888576a6e8f58efcebfdf83ff686152abf8
Cat Hanbit <ryudb030@gmail.com>
2024-03-14 14:34:38 +0900

    URL 클래스 생성
    GET 요청으로 올때 가운데 타겟 URI를 추출해서 반환해준다.
    그 요청 덩어리를 logger를 통해 끝까지 출력해준다.

---
+++ src/main/java/util/URL.java
@@ -0,0 +1,35 @@
+package util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class URL {
+    private static final Logger logger = LoggerFactory.getLogger(URL.class);
+
+//    GET /index.html HTTP/1.1 들어오면 GET과 HTTP/1.1 사이의 값을 추출함
+    public static String getTargetURI(BufferedReader br) throws IOException {
+        String firstRequest = br.readLine();
+        logger.debug("firstRequest : {}", firstRequest);
+
+//        allRequest(br, firstRequest);
+        
+        String[] split = firstRequest.split(" ");
+        String uri = split[1];
+        return uri;
+    }
+
+//    요청을 모두 받는다.
+    private static void allRequest(BufferedReader br, String request) throws IOException{
+        while (!request.isEmpty()) {
+            logger.debug("request : {}", request);
+            request = br.readLine();
+        }
+    }
+
+
+}
