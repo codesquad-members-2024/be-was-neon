4c8d2a51b9bd3c13b5232de9275e8cf12f1b786b
Daniel <923daniel@naver.com>
2024-03-14 17:07:58 +0900

    feat: HttpRequest 클래스 구현
    - InputStream을 받는다.
    - 클래스 내부에서 startLine 파싱.

---
+++ src/main/java/webserver/HttpRequest.java
@@ -0,0 +1,53 @@
+package webserver;
+
+import java.io.*;
+
+public class HttpRequest {
+    private static final String BASIC_FILE_PATH = "src/main/resources/static";
+    private static final String INDEX_FILE_NAME = "/index.html";
+
+    private String startLine;
+    private String method;
+    private String url;
+    private String version;
+
+
+    HttpRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        this.startLine = br.readLine();
+
+
+    }
+
+    private void parseStartLine(String startLine){
+        String[] splitStartLine = startLine.split(" ");
+        this.method = splitStartLine[0];
+        this.url = splitStartLine[1];
+        this.version = splitStartLine[2];
+    }
+
+    public String getCompletePath(){
+        StringBuilder completePath = new StringBuilder(BASIC_FILE_PATH);
+        if(!url.equals(INDEX_FILE_NAME) && !url.contains(RequestHandler.REGISTER_ACTION)){
+            completePath.append(url);
+        }
+        completePath.append(INDEX_FILE_NAME);
+        return completePath.toString();
+    }
+
+    public String getStartLine(){
+        return startLine;
+    }
+
+    public String getMethod(){
+        return method;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public String getVersion(){
+        return version;
+    }
+}
