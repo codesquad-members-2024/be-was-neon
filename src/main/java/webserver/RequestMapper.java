88a7777996eb92bb0a3693a21bb27a671e8c4524
Keon Kim <fnc6488@gmail.com>
2024-03-14 11:49:59 +0900

    RequestHandler 리팩토링
    `HttpRequest`를 인자로 받아 `HttpResponse`를 반환하도록 변경
    `Map<String, Supplier>`를 Map<String, Function>으로 변경
    `RequestHandler` 에 존재하던 유저 생성 로직을 `RequestMapper`로의 `createUser`메서드로 이동

--- src/main/java/webserver/RequestMapper.java
+++ src/main/java/webserver/RequestMapper.java
@@ -1,41 +1,62 @@
 package webserver;
 
+import db.Database;
+import model.User;
+import webserver.httpMessage.HttpRequest;
 import webserver.httpMessage.HttpResponse;
+import webserver.utils.Parser;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 public class RequestMapper {
 
     private static final String STATIC_PATH = "src/main/resources/static";
     private static final String INDEX_HTML = "/index.html";
 
-    private final Map<String, Supplier<HttpResponse>> map = Map.ofEntries(
+    private final Map<String, Function<HttpRequest, HttpResponse>> map = Map.ofEntries(
             Map.entry("/", this::home),
             Map.entry("/registration", this::register),
-            Map.entry("/login", this::login)
+            Map.entry("/login", this::login),
+            Map.entry("/create", this::createUser)
     );
 
-    public HttpResponse getHttpResponse(String requestTarget) {
-        if (requestTarget.contains(".")) {
-            return HttpResponse.from(new File(STATIC_PATH + requestTarget));
+    public HttpResponse service(HttpRequest request) {
+        String uri = request.getUri();
+        System.out.println(uri);
+        if (map.containsKey(uri)) {
+            return map.get(uri).apply(request);
         }
-        return map.get(requestTarget).get();
+        return HttpResponse.from(new File(STATIC_PATH + uri));
     }
 
-    private HttpResponse home() {
+    private HttpResponse home(HttpRequest request) {
         File file = new File(STATIC_PATH + INDEX_HTML);
         return HttpResponse.from(file);
     }
 
-    private HttpResponse register() {
+    private HttpResponse register(HttpRequest request) {
         File file = new File(STATIC_PATH + "/registration" + INDEX_HTML);
         return HttpResponse.from(file);
     }
 
-    private HttpResponse login() {
+    private HttpResponse login(HttpRequest request) {
         File file = new File(STATIC_PATH + "/login" + INDEX_HTML);
         return HttpResponse.from(file);
+    }
+
+    private HttpResponse createUser(HttpRequest request) {
+        String queryParams = request.getQueryParams();
+        System.out.println(queryParams);
+        try {
+            Map<String, String> userForm = Parser.splitQuery(queryParams);
+            Database.addUser(User.from(userForm));
+            File file = new File(STATIC_PATH + "/login" + INDEX_HTML);
+            return HttpResponse.from(file);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
