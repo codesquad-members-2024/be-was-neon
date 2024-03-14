c69aee65e80add9179dd73ea86c66627d4b7c3f4
Daniel <923daniel@naver.com>
2024-03-15 07:27:34 +0900

    feat: HttpResponseHeader 클래스 구현
    - response header 부분만 구성해서 전달.

---
+++ src/main/java/webserver/HttpResponseHeader.java
@@ -0,0 +1,49 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class HttpResponseHeader {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponseHeader.class);
+    private DataOutputStream dos;
+
+    HttpResponseHeader(DataOutputStream dos){
+        this.dos = dos;
+    }
+
+    public void setStartLine(String statusCode, String statusMessage){
+        try {
+            dos.writeBytes("HTTP/1.1 " + statusCode + " " +statusMessage + "\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void setContentType(String contentType) {
+        try {
+            dos.writeBytes("Content-Type: " + contentType + "\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void setLocation(String location) {
+        try {
+            dos.writeBytes("Location: " + location + "\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void setLengthOfBodyContent(int contentLength) {
+        try {
+            dos.writeBytes("Content-Length: " + contentLength + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
