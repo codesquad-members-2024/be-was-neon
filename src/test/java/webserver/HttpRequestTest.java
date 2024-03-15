bb676e3a369792ae0eccfc50fb3a5c0288f39bcf
Daniel <923daniel@naver.com>
2024-03-15 11:54:52 +0900

    feat: HttpRequestTest 회원정보 테스트 추가
    - parseRegisterInfo 매소드는 회원가입 정보 파싱 테스트.
    - storeUserAtDb 매소드는 user객체를 생성해 db에 자장 테스트.

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,5 +1,7 @@
 package webserver;
 
+import db.Database;
+import model.User;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,7 +10,7 @@ public class HttpRequestTest {
     private HttpRequest httpRequest;
 
     @BeforeEach
-    void setUp() {
+    void setHttpRequest() {
         httpRequest = new HttpRequest("GET /registration HTTP/1.1");
     }
 
@@ -31,9 +33,37 @@ public class HttpRequestTest {
     }
 
     @Test
-    @DisplayName("static 폴더에 있는 파일의 경로를 반환해야 한다")
+    @DisplayName("static 폴더에 있는 파일의 경로를 반환해야 한다.")
     void getRegisterPath() {
         String completePath = httpRequest.getCompletePath();
         assertThat(completePath).isEqualTo("src/main/resources/static/registration/index.html");
+    }
+
+    @Test
+    @DisplayName("StartLine에서 RequestParameter를 추출 한다.")
+    void parseRegisterInfo() {
+        String requestStartLine = "GET /user/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1";
+        httpRequest = new HttpRequest(requestStartLine);
+        httpRequest.parseRegisterData();
+
+        assertThat(httpRequest.getUserId()).isEqualTo("javajigi");
+        assertThat(httpRequest.getPassword()).isEqualTo("password");
+        assertThat(httpRequest.getName()).isEqualTo("박재성");
+        assertThat(httpRequest.getEmail()).isEqualTo("javajigi@slipp.net");
+    }
+
+    @Test
+    @DisplayName("StartLine에서 RequestParameter를 추출한 후, user 객체를 DB에 저장해야 한다.")
+    void storeUserAtDb() {
+        String requestStartLine = "GET /user/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1";
+        httpRequest = new HttpRequest(requestStartLine);
+        httpRequest.parseRegisterData();
+        httpRequest.storeDatabase();
+
+        User user = Database.findUserById("javajigi");
+        assertThat(user.getUserId()).isEqualTo("javajigi");
+        assertThat(user.getPassword()).isEqualTo("password");
+        assertThat(user.getName()).isEqualTo("박재성");
+        assertThat(user.getEmail()).isEqualTo("javajigi@slipp.net");
     }
 }
