e5567ca7d50e2a9631c1a275fd2225d3912289e3
DESKTOP-GDDP7GQ\User <ogt124321@gmail.com>
2024-03-18 01:27:27 +0900

    리펙토링 RequestHandler의 역할을 덜어주고 HttpRequest를 static 이 아닌 객체를 만들어 사용할수있게 변경해주었다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,7 +2,6 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +21,12 @@ public class RequestHandler implements Runnable {
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String requestLine = br.readLine();
-            // httpRequest 와 header 들을 읽어와 logger 로 console 에 출력해주었습니다.
-            String requestURL = HttpRequest.getURL(requestLine);
-            //HttpRequest.getRequestHeader(br);
+            HttpRequest httpRequest = new HttpRequest();
+            httpRequest.parseRequestLines(in);
 
             DataOutputStream dos = new DataOutputStream(out);
             // httpRequest 의 타입에 따라 다른 response 를 보내줄수있게 해주었습니다.
-            RequestHandler.handleRequest(requestURL,dos);
+            RequestHandler.handleRequest(httpRequest.getRequestType(),dos);
 
         } catch (IOException e) {
             logger.error(e.getMessage());
