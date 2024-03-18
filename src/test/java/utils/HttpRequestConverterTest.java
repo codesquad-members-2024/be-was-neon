62757bef74f04c5355f5903702e651371eb2394e
Yeriimii <seojs0511@naver.com>
2024-03-19 01:06:49 +0900

    refactor: HttpRequest 리팩터링
    - 필드의 요청 헤더 -> Map 으로 변환

--- src/test/java/utils/HttpRequestConverterTest.java
+++ src/test/java/utils/HttpRequestConverterTest.java
@@ -38,20 +38,20 @@ class HttpRequestConverterTest {
         assertThat(httpRequest.getHttpVersion()).isEqualTo("HTTP/1.1");
 
         /* headers */
-        assertThat(httpRequest.getHost()).isEqualTo("Host: localhost:8080");
-        assertThat(httpRequest.getConnection()).isEqualTo("Connection: keep-alive");
-        assertThat(httpRequest.getReferer()).isEqualTo("Referer: \"https://www.google.com/\"");
-        assertThat(httpRequest.getUserAgent()).isEqualTo("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+        assertThat(httpRequest.getHeader("Host")).isEqualTo("localhost:8080");
+        assertThat(httpRequest.getHeader("Connection")).isEqualTo("keep-alive");
+        assertThat(httpRequest.getHeader("Referer")).isEqualTo("\"https://www.google.com/\"");
+        assertThat(httpRequest.getHeader("User-Agent")).isEqualTo("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
 
-        assertThat(httpRequest.getAccept()).isEqualTo("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
-        assertThat(httpRequest.getAcceptEncoding()).isEqualTo("Accept-Encoding: gzip, deflate, br, zstd");
-        assertThat(httpRequest.getAcceptLanguage()).isEqualTo("Accept-Language: ko,en-US;q=0.9,en;q=0.8,ko-KR;q=0.7");
+        assertThat(httpRequest.getHeader("Accept")).isEqualTo("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+        assertThat(httpRequest.getHeader("Accept-Encoding")).isEqualTo("gzip, deflate, br, zstd");
+        assertThat(httpRequest.getHeader("Accept-Language")).isEqualTo("ko,en-US;q=0.9,en;q=0.8,ko-KR;q=0.7");
 
-        assertThat(httpRequest.getCookie()).isEqualTo("Cookie: Idea-fcd223d4=b6a3f2fa-6bf3-46d9-9244-dc44850cb75f;");
-        assertThat(httpRequest.getCacheControl()).isEqualTo("Cache-Control: no-cache");
-        assertThat(httpRequest.getPragma()).isEqualTo("Pragma: no-cache");
-        assertThat(httpRequest.getIfModifiedSince()).isEqualTo("If-Modified-Since: none;");
-        assertThat(httpRequest.getIfNoneMatch()).isEqualTo("If-None-Match: none;");
+        assertThat(httpRequest.getHeader("Cookie")).isEqualTo("Idea-fcd223d4=b6a3f2fa-6bf3-46d9-9244-dc44850cb75f;");
+        assertThat(httpRequest.getHeader("Cache-Control")).isEqualTo("no-cache");
+        assertThat(httpRequest.getHeader("Pragma")).isEqualTo("no-cache");
+        assertThat(httpRequest.getHeader("If-Modified-Since")).isEqualTo("none;");
+        assertThat(httpRequest.getHeader("If-None-Match")).isEqualTo("none;");
     }
 
     @DisplayName("post 요청일 때 http body인 'id=yelly&password=myPassword&username=testName&email=test@test.com'를 쿼리 파라미터로 가져올 수 있다")
