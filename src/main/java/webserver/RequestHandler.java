5cdc2511feefa6db63d30b25d32dec51f3b893f8
Daniel <923daniel@naver.com>
2024-03-19 00:35:29 +0900

    refactor: HttpRequest 클래스 body 파싱 매소드 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -39,6 +39,13 @@ public class RequestHandler implements Runnable {
                 requestLine = br.readLine();
             }
 
+            if(httpRequest.getMethod().equals("POST")){
+                char[] buffer = new char[httpRequest.getContentLength()]; // 읽을 바이트 수
+                br.read(buffer, 0, buffer.length); // 특정 바이트 수만큼 읽기
+                String result = new String(buffer);
+                System.out.println("읽은 문자열: " + result);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             sendResponse(dos, httpRequest);
 
@@ -57,32 +64,39 @@ public class RequestHandler implements Runnable {
             byte[] fileContent = fis.readAllBytes();
             fis.close();
 
-            if(httpRequest.checkRegisterDataEnter()){
-                httpResponseHeader.setStartLine("302", "FOUND");
-                httpResponseHeader.setLocation("/index.html"); // redirect 경로 지정
-            }else{
-                httpResponseHeader.setStartLine("200", "OK");
-            }
-            httpResponseHeader.setContentType(ContentType.getContentType(httpRequest.getFileType()));
+//            if(httpRequest.checkRegisterDataEnter()){
+//                httpResponseHeader.setStartLine("302", "FOUND");
+//                httpResponseHeader.setLocation("/index.html"); // redirect 경로 지정
+//            }else{
+//                httpResponseHeader.setStartLine("200", "OK");
+//            }
+            httpResponseHeader.setStartLine("200", "OK"); //삭제
+            httpResponseHeader.setContentType(ContentType.getContentType(getFileType(GetPath.getCompletePath(httpRequest.getUrl()))));
             httpResponseHeader.setContentLength(fileContent.length);
             httpResponseBody.setBody(fileContent);
         }else{
             byte[] fileContent = "<h1>404 Not Found</h1>".getBytes();
 
             httpResponseHeader.setStartLine("404", "Not Found");
-            httpResponseHeader.setContentType(ContentType.getContentType(httpRequest.getFileType()));
+            httpResponseHeader.setContentType(ContentType.getContentType(getFileType(GetPath.getCompletePath(httpRequest.getUrl()))));
             httpResponseHeader.setContentLength(fileContent.length);
             httpResponseBody.setBody(fileContent);
         }
         dos.flush();
     }
 
-    private void storeUser(HttpRequest httpRequest){
-        httpRequest.parseRegisterData();
-        httpRequest.storeDatabase();
-    }
+//    private void storeUser(HttpRequest httpRequest){
+//        httpRequest.parseRegisterData();
+//        httpRequest.storeDatabase();
+//    }
 
     private boolean checkValidFile(File file){
         return (file.exists() && !file.isDirectory());
     }
+
+    public String getFileType(String getCompletePath){
+        String[] splitPath = getCompletePath.split("\\.");
+        return splitPath[1]; // .으로 split 했을 때 idx:1이 타입
+    }
+
 }
