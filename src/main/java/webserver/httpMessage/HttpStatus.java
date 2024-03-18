29cd8a7fa3a4645348dc3f3c8000d5f48fdb2db4
Keon Kim <fnc6488@gmail.com>
2024-03-18 17:58:36 +0900

    redirect 상태 코드를 303으로 변경
    회원가입 이후 POST 요청 메서드 본문 제거를 위해 HTTP 응답 코드를 307에서 303으로 변경

--- src/main/java/webserver/httpMessage/HttpStatus.java
+++ src/main/java/webserver/httpMessage/HttpStatus.java
@@ -2,7 +2,8 @@ package webserver.httpMessage;
 
 public enum HttpStatus {
     OK("HTTP/1.1 200 OK \r\n"),
-    TEMPORARY_REDIRECTION("HTTP/1.1 307 Temporary Redirect \r\n");
+    TEMPORARY_REDIRECTION("HTTP/1.1 307 Temporary Redirect \r\n"),
+    SEE_OTHER("HTTP/1.1 303 See Other \r\n");
 
     private final String statusMessage;
 
