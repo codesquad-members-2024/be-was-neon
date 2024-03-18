8198af96df528b9ae50ee9e46322cbcf70a926d8
Daniel <923daniel@naver.com>
2024-03-18 15:18:17 +0900

    refactor: HttpRequest 클래스 재구성
    - headers의 정보를 headersData hash map에 저장.
    - HttpRequest에 필요없는 내용 주석처리.

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -8,106 +8,116 @@ import java.net.URLDecoder;
 import java.util.HashMap;
 
 public class HttpRequest {
-    private static final String BASIC_FILE_PATH = "src/main/resources/static";
-    private static final String INDEX_FILE_NAME = "/index.html";
-    public static final String REGISTER_ACTION = "/user/create";
+//    private static final String BASIC_FILE_PATH = "src/main/resources/static";
+//    private static final String INDEX_FILE_NAME = "/index.html";
+//    public static final String REGISTER_ACTION = "/user/create";
     public static final String METHOD_GET = "GET";
     private static final String DOT = "\\.";
+    private static final String SPACE = " ";
+    private static final String EQUAL = "=";
+
 
     private String startLine;
-    private String method;
-    private String url;
-    private String version;
 
-    private HashMap<String, String> registerUserData;
+    private HashMap<String, String> startLineData;
+    private HashMap<String, String> headersData;
 
-    HttpRequest(String startLine) {
+    HttpRequest() {
+        this.startLineData = new HashMap<String, String>();
+        this.headersData = new HashMap<String, String>();
+    }
+
+    public void storeStartLineData(String startLine){
         this.startLine = startLine;
-        this.registerUserData = new HashMap<String, String>();
         parseStartLine(startLine);
     }
 
-    // 회원가입 정보 파싱
-    public void parseRegisterData() {
-        for(String token : extractUserData().split("[& ]")){
-            String[] splitInfo = token.split("="); // 이름과 값을 = 로 분리
-            try {
-                registerUserData.put(splitInfo[0], URLDecoder.decode(splitInfo[1], "UTF-8")); // 해쉬 맵에 정보 저장
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        }
+    public void storeHeadersData(String headerLine){ // headers 한줄마다 HashMap에 저장
+        String[] splitHeaderLine = headerLine.split(EQUAL);
+        headersData.put(splitHeaderLine[0], splitHeaderLine[1]);
     }
 
     // 회원가입 정보 파싱
-    private String extractUserData(){ // 사용자 입력 정보 부분만 반환
-        String[] splitStartLine = url.split("[?]");
-        return splitStartLine[1];
-    }
+//    public void parseRegisterData() {
+//        for(String token : extractUserData().split("[& ]")){
+//            String[] splitInfo = token.split("="); // 이름과 값을 = 로 분리
+//            try {
+//                registerUserData.put(splitInfo[0], URLDecoder.decode(splitInfo[1], "UTF-8")); // 해쉬 맵에 정보 저장
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    // 회원가입 정보 파싱
+//    private String extractUserData(){ // 사용자 입력 정보 부분만 반환
+//        String[] splitStartLine = url.split("[?]");
+//        return splitStartLine[1];
+//    }
 
     // 회원가입 데이터 DB에 추가
-    public void storeDatabase(){
-        Database.addUser(new User(registerUserData.get("userId"), registerUserData.get("password"), registerUserData.get("name"), registerUserData.get("email")));
-    }
+//    public void storeDatabase(){
+//        Database.addUser(new User(registerUserData.get("userId"), registerUserData.get("password"), registerUserData.get("name"), registerUserData.get("email")));
+//    }
 
-    public boolean checkRegisterDataEnter(){ // 회원가입에서 보낸 GET인지 확인
-        return (method.equals(METHOD_GET) && url.startsWith(REGISTER_ACTION));
-    }
+//    public boolean checkRegisterDataEnter(){ // 회원가입에서 보낸 GET인지 확인
+//        return (method.equals(METHOD_GET) && url.startsWith(REGISTER_ACTION));
+//    }
 
 
     private void parseStartLine(String startLine){
-        String[] splitStartLine = startLine.split(" ");
-        this.method = splitStartLine[0];
-        this.url = splitStartLine[1];
-        this.version = splitStartLine[2];
+        String[] splitStartLine = startLine.split(SPACE);
+        startLineData.put("method", splitStartLine[0]);
+        startLineData.put("url", splitStartLine[1]);
+        startLineData.put("version", splitStartLine[2]);
     }
 
-    public String getCompletePath(){ // /registraion
-        StringBuilder completePath = new StringBuilder(BASIC_FILE_PATH);
-        if(!url.contains(REGISTER_ACTION)){
-            completePath.append(url);
-        }
-        File file = new File(completePath.toString());
-        if(file.isDirectory()){ // file이 아니라 폴더이면 "/index.html" 추가
-            completePath.append(INDEX_FILE_NAME);
-        }
-        return completePath.toString();
-    }
+//    public String getCompletePath(){ // /registraion
+//        StringBuilder completePath = new StringBuilder(BASIC_FILE_PATH);
+//        if(!url.contains(REGISTER_ACTION)){
+//            completePath.append(url);
+//        }
+//        File file = new File(completePath.toString());
+//        if(file.isDirectory()){ // file이 아니라 폴더이면 "/index.html" 추가
+//            completePath.append(INDEX_FILE_NAME);
+//        }
+//        return completePath.toString();
+//    }
 
-    public String getFileType(){
-        String[] splitPath = getCompletePath().split(DOT);
-        return splitPath[1]; // .으로 split 했을 때 idx:1이 타입
-    }
+//    public String getFileType(){
+//        String[] splitPath = getCompletePath().split(DOT);
+//        return splitPath[1]; // .으로 split 했을 때 idx:1이 타입
+//    }
 
     public String getStartLine(){
         return startLine;
     }
 
     public String getMethod(){
-        return method;
+        return startLineData.get("method");
     }
 
     public String getUrl(){
-        return url;
+        return startLineData.get("url");
     }
 
     public String getVersion(){
-        return version;
+        return startLineData.get("version");
     }
 
-    public String getUserId(){
-        return registerUserData.get("userId");
-    }
-
-    public String getPassword(){
-        return registerUserData.get("password");
-    }
-
-    public String getName(){
-        return registerUserData.get("name");
-    }
-
-    public String getEmail(){
-        return registerUserData.get("email");
-    }
+//    public String getUserId(){
+//        return registerUserData.get("userId");
+//    }
+//
+//    public String getPassword(){
+//        return registerUserData.get("password");
+//    }
+//
+//    public String getName(){
+//        return registerUserData.get("name");
+//    }
+//
+//    public String getEmail(){
+//        return registerUserData.get("email");
+//    }
 }
