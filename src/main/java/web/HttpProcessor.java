5147a90a520ffdd4abbbbcc7fd833b9ef2d4a000
Yeriimii <seojs0511@naver.com>
2024-03-18 23:37:37 +0900

    refactor: HtmlProcessor 리팩터링
    - 매직 리터럴 상수화

--- src/main/java/web/HttpProcessor.java
+++ src/main/java/web/HttpProcessor.java
@@ -4,6 +4,8 @@ import http.HttpRequest;
 import http.HttpResponse;
 
 public abstract class HttpProcessor {
+    public static final String BASIC_HTTP_VERSION = "HTTP/1.1";
+    public static final String BASIC_CHAR_SET = "utf-8";
 
     abstract void process(HttpRequest request, HttpResponse response);
 
