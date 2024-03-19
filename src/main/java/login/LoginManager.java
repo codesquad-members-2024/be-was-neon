ba98751437b187c8d5c69fbd8ab293ccf0c82cef
Yeriimii <seojs0511@naver.com>
2024-03-19 18:58:30 +0900

    refactor: LoginManager 리팩터링
    - 검증 로직을 아이디 -> 패스워드 순으로 검증하고 성공하면 유저를 반환하는 로직으로 수정

--- src/main/java/login/LoginManager.java
+++ src/main/java/login/LoginManager.java
@@ -12,16 +12,20 @@ public class LoginManager {
     public Optional<User> login(String id, String password) {
         Optional<User> optionalUser = Database.findUserById(id);
 
+        /* 유저 아이디 검증 */
         if (optionalUser.isEmpty()) {
             logger.debug("[LOGIN MANAGER] Not Found Id = {}", id);
             return Optional.empty();
         }
 
+        /* 패스워드 검증 */
         User user = optionalUser.get();
-        if (user.getPassword().equals(password)) {
-            return Optional.of(user);
+        if (!user.getPassword().equals(password)) {
+            logger.debug("[LOGIN MANAGER] password error = {}", password);
+            return Optional.empty();
         }
-        logger.debug("[LOGIN MANAGER] password error = {}", password);
-        return Optional.empty();
+
+        /* 유저 반환 */
+        return Optional.of(user);
     }
 }
