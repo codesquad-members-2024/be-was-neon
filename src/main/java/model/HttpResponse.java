08abd19ef5fdd954a56840ae8e71dd410a9476a1
sean <tmdgus717@naver.com>
2024-03-19 13:42:26 +0900

    HttpRequest, HttpRequestBuilder, ResponseHandler 구현
    작업에 필요한 HTTP REQUEST 정보를 갖고 있는 HttpRequest 클래스와 클래스의 필드 값을 생성하는 HttpRequestBuilder 클래스를 구현함
    RequestHandler에서 파일을 반환하거나 회원가입을 하는 작업을 분리한 ResponseHandler 클래스생성

---
+++ src/main/java/model/HttpResponse.java
@@ -0,0 +1,11 @@
+package model;
+
+public class HttpResponse {
+    private static final String httpVersion = null;
+    private static final String statusCode = null;
+    private static final String statusText = null;
+
+    public HttpResponse() {
+    }
+
+}
