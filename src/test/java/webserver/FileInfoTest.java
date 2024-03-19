069647139dc960dacac6ed8b6efd179d1c74ea67
Daniel <923daniel@naver.com>
2024-03-19 10:27:55 +0900

    FileInfoTest 테스트 클래스 구현

---
+++ src/test/java/webserver/FileInfoTest.java
@@ -0,0 +1,21 @@
+package webserver;
+
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileInfoTest {
+
+    @Test
+    @DisplayName("static 폴더에 있는 파일의 경로를 반환해야 한다.")
+    void getCompletePath() {
+        String completePath = FileInfo.getCompletePath("/registration");
+        assertThat(completePath).isEqualTo("src/main/resources/static/registration/index.html");
+    }
+
+    @Test
+    @DisplayName("파일 확장자를 반환해야 한다.")
+    void getFileType(){
+        String fileType = FileInfo.getFileType("src/main/resources/static/registration/index.html");
+        assertThat(fileType).isEqualTo("html");
+    }
+}
