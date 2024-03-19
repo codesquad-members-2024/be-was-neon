6f6b11ef260b50aa87a8ee498d6ba12d0032a852
Daniel <923daniel@naver.com>
2024-03-19 10:44:41 +0900

    feat: ContentTypeTest 테스트 클래스 구현
    - 파일에 맞는 ContentType을 반환해야 한다.

---
+++ src/test/java/webserver/ContentTypeTest.java
@@ -0,0 +1,27 @@
+package webserver;
+
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContentTypeTest {
+    @Test
+    @DisplayName("html 파일이면 ContentType으로 'text/html;charset=utf-8'을 반환 한다.")
+    void getHtmlContentType() {
+        String completePath = "src/main/resources/static/registration/index.html";
+        assertThat(ContentType.getContentType(FileInfo.getFileType(completePath))).isEqualTo("text/html;charset=utf-8");
+    }
+
+    @Test
+    @DisplayName("css 파일이면 ContentType으로 'text/css;charset=utf-8'을 반환 한다.")
+    void getCssContentType() {
+        String completePath = "src/main/resources/static/main.css";
+        assertThat(ContentType.getContentType(FileInfo.getFileType(completePath))).isEqualTo("text/css;charset=utf-8");
+    }
+
+    @Test
+    @DisplayName("svg 파일이면 ContentType으로 'image/svg+xml'을 반환 한다.")
+    void getSvgContentType() {
+        String completePath = "src/main/resources/static/img/signiture.svg";
+        assertThat(ContentType.getContentType(FileInfo.getFileType(completePath))).isEqualTo("image/svg+xml");
+    }
+}
