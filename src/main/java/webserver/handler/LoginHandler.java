f35c110d89aafc3d7b2ca786c5a2fe42ee3223c2
Keon Kim <fnc6488@gmail.com>
2024-03-20 18:42:48 +0900

    로그인에 성공하면 헤더에 쿠키를 추가하도록 변경
    로그인에 성공하면 응답 헤더 쿠키에 UUID로 세션 ID를 추가

--- src/main/java/webserver/handler/LoginHandler.java
+++ src/main/java/webserver/handler/LoginHandler.java
@@ -9,6 +9,7 @@ import webserver.httpMessage.HttpResponse;
 import webserver.utils.HttpRequestParser;
 
 import java.util.Map;
+import java.util.UUID;
 
 import static webserver.handler.Path.HOME_PAGE;
 import static webserver.handler.Path.LOGIN_FAILED_PAGE;
@@ -26,16 +27,22 @@ public class LoginHandler implements Handler{
         String password = loginQuery.get("password");
 
         User user = Database.findUserById(id);
+        HttpResponse response = new HttpResponse();
         if (user == null) {
             logger.info("존재하지 않는 ID");
-            return HttpResponse.redirect(LOGIN_FAILED_PAGE.getPath());
+            response.redirect(LOGIN_FAILED_PAGE.getPath());
+            return response;
         }
         if (!user.getPassword().equals(password)) {
             logger.info("비밀번호 불일치 {}, {}", user.getPassword(), password);
-            return HttpResponse.redirect(LOGIN_FAILED_PAGE.getPath());
+            response.redirect(LOGIN_FAILED_PAGE.getPath());
+            return response;
         }
-
         logger.info("{} : 로그인 성공", user.getUserId());
-        return HttpResponse.redirect(HOME_PAGE.getPath());
+
+        response.redirect(HOME_PAGE.getPath());
+        UUID uuid = UUID.randomUUID();
+        response.setCookie(uuid);
+        return response;
     }
 }
