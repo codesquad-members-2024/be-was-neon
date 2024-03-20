466d78b245e283f68bd6a8bfc83008c3a6be5c85
Keon Kim <fnc6488@gmail.com>
2024-03-20 11:07:05 +0900

    HttpRequestParserTest 변경
    `parseQueryParams` 테스트 코드의 실제 값을 인코딩 된 문자열로 변경

--- src/test/java/webserver/utils/HttpRequestParserTest.java
+++ src/test/java/webserver/utils/HttpRequestParserTest.java
@@ -11,7 +11,7 @@ class HttpRequestParserTest {
     @Test
     @DisplayName("쿼리 파라미터를 Map으로 변환")
     void parseQueryParams() throws UnsupportedEncodingException {
-        String query = "userId=cori123&password=1234&name=cori&email=cori%40naver.com";
+        String query = "userId=cori123&password=1234&name=cori&email=cori@naver.com";
         Map<String, String> userForm = HttpRequestParser.parseKeyValuePairs(query);
 
         Map<String, String> expected = Map.of(
