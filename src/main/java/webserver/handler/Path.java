c18eb389e02e3505b4e683206b3e51b9be088c6f
Keon Kim <fnc6488@gmail.com>
2024-03-20 16:57:38 +0900

    `Path` 열거형 경로 수정
    `Path`에 정적 파일의 상대경로를 얻는 메서드 구현

--- src/main/java/webserver/handler/Path.java
+++ src/main/java/webserver/handler/Path.java
@@ -1,14 +1,24 @@
 package webserver.handler;
 
 public enum Path {
-    STATIC_PATH("src/main/resources/static"),
-    INDEX_HTML("/index.html"),
-    REGISTRATION("/registration"),
-    LOGIN("/login");
+    RESOURCE_PATH("src/main/resources/static"),
+    HOME_PAGE("/index.html"),
+    REGISTRATION_PAGE("/registration/index.html"),
+    LOGIN_PAGE("/login/index.html"),
+    LOGIN_FAILED_PAGE("/login/login_failed.html");
 
-    final String path;
+    private static final String staticPath = "src/main/resources/static";
+    private final String path;
 
     Path(String s) {
         this.path = s;
+    }
+    
+    public String getRelativePath() {
+        return staticPath + path; 
+    }
+
+    public String getPath() {
+        return path;
     }
 }
--- src/main/java/webserver/handler/StaticFileHandler.java
+++ src/main/java/webserver/handler/StaticFileHandler.java
@@ -5,26 +5,15 @@ import webserver.httpMessage.HttpResponse;
 
 import java.io.File;
 
-import static webserver.handler.Path.INDEX_HTML;
-import static webserver.handler.Path.STATIC_PATH;
+import static webserver.handler.Path.RESOURCE_PATH;
 
 public class StaticFileHandler implements Handler {
 
     @Override
     public HttpResponse service(HttpRequest request) {
         String uri = request.getUri();
-        File file = findFile(uri);
+        File file = new File(RESOURCE_PATH.getPath() + uri);
         return HttpResponse.from(file);
     }
 
-    private File findFile(String uri) {
-        if (isNotStaticFile(uri)) {
-            uri += INDEX_HTML.path;
-        }
-        return new File(STATIC_PATH.path + uri);
-    }
-
-    private static boolean isNotStaticFile(String uri) {
-        return !uri.contains(".");
-    }
 }
