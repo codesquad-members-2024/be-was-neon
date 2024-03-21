394bad08598700f69257be44effe2684492cdee5
Keon Kim <fnc6488@gmail.com>
2024-03-21 11:36:51 +0900

    `/user/list` 접속 시 로그인 여부를 확인하는 기능 구현
    `HttpRequest` 객체로부터 세션 아이디를 가져오는 메서드 정의
    로그인하지 않았을 경우 홈 화면으로 리다이렉트
    로그인했을 경우 사용자 목록 페이지를 보여준다.

--- src/main/java/webserver/handler/UserListHandler.java
+++ src/main/java/webserver/handler/UserListHandler.java
@@ -2,19 +2,27 @@ package webserver.handler;
 
 import db.Database;
 import model.User;
+import webserver.Session.SessionManger;
 import webserver.httpMessage.HttpRequest;
 import webserver.httpMessage.HttpResponse;
 import webserver.httpMessage.HttpStatus;
 
 import java.util.Collection;
 
+import static webserver.handler.Path.HOME_PAGE;
+
 public class UserListHandler implements Handler {
 
     @Override
     public HttpResponse service(HttpRequest request) {
+        HttpResponse response = new HttpResponse();
         Collection<User> users = Database.findAll();
 
-        HttpResponse response = new HttpResponse();
+        String sid = request.getSessionId();
+        if (!SessionManger.isLogin(sid)) {
+            response.redirect(HOME_PAGE.getPath());
+        }
+
         response.setStatus(HttpStatus.OK);
         response.setBody(getUserListHtml(users).getBytes());
 
