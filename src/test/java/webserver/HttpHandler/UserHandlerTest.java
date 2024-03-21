d7f35a76ef0755e3b3d7cd34ab64d1d01b29b865
Miensoap <miensop51@gmail.com>
2024-03-21 14:50:19 +0900

    DynamicTest 사용 중복 가입 시나리오 테스트 작성

--- src/test/java/webserver/HttpHandler/UserHandlerTest.java
+++ src/test/java/webserver/HttpHandler/UserHandlerTest.java
@@ -1,46 +1,47 @@
 package webserver.HttpHandler;
 
 import db.Database;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import webserver.HttpMessage.Request;
 import webserver.HttpMessage.Response;
 import webserver.TestUtils;
 
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static org.assertj.core.api.Assertions.*;
 
 class UserHandlerTest {
 
     final UserHandler userHandler = new UserHandler();
 
     @AfterEach
-    void clearDB(){
+    void clearDB() {
         Database.clear();
     }
-    @Test
-    @DisplayName("createUser 요청이 들어오면 유저가 DB에 추가되고 , 302 응답을 보낸다")
-    void createUserTest() {
+
+    @DisplayName("같은 ID 가입 두번 시나리오")
+    @TestFactory
+    Collection<DynamicTest> doubleRegistration() {
         Request request = TestUtils.createUserRequest;
-        Response response = userHandler.createUser(request);
+        return List.of(
+                DynamicTest.dynamicTest("createUser 요청이 들어오면 유저가 DB에 추가되고 , 302 응답을 보낸다", () -> {
+                    Response response = userHandler.createUser(request);
 
-        assertSoftly(softly -> {
-            softly.assertThat(response.getStartLine().toString()).isEqualTo("HTTP/1.1 302 Found");
-            softly.assertThat(Database.findUserById("test").getName()).isEqualTo("test");
-        });
+                    assertSoftly(softly -> {
+                        softly.assertThat(response.getStartLine().toString()).isEqualTo("HTTP/1.1 302 Found");
+                        softly.assertThat(Database.findUserById("test").getName()).isEqualTo("test");
+                    });
+                }),
+
+                DynamicTest.dynamicTest("이미 존재하는 유저 ID 라면 DB에 추가하지 않는다", () -> {
+                    int beforeSize = Database.findAll().size();
+
+                    userHandler.createUser(request);
+
+                    assertThat(Database.findAll().size()).isEqualTo(beforeSize);
+                })
+        );
     }
-
-    @Test
-    @DisplayName("이미 존재하는 유저 ID 라면 DB에 추가하지 않는다")
-    void FailToCreateUser() {
-        Request request = TestUtils.createUserRequest;
-        userHandler.createUser(request);
-        int beforeSize = Database.findAll().size();
-
-        userHandler.createUser(request);
-
-        assertThat(Database.findAll().size()).isEqualTo(beforeSize);
-    }
-
 }
