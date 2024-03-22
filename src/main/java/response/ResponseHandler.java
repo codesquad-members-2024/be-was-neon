6959d2dcf2777460ee82bf387bd2b0908e9bb715
Gyeongtaek <ogt124321@gmail.com>
2024-03-22 09:57:10 +0900

    Response 의 코드 리펙토링 및 쿠키 처리 코드 추가 쿠키를 처리해주기위한 코드와 그에 따른 전체적인 Response 관련 코드를 수정해주었습니다.

--- src/main/java/response/ResponseHandler.java
+++ src/main/java/response/ResponseHandler.java
@@ -1,12 +1,19 @@
 package response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import request.HttpRequest;
+import session.SessionManager;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class ResponseHandler {
     private final String RELATIVE_PATH = "./src/main/resources/static";
+    private static final String MAIN_PAGE_URL = "/index.html";
+    private static final String LOGIN_FAIL_PAGE_URL = "/login/login_failed.html";
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private final HttpRequest httpRequest;
     public ResponseHandler(HttpRequest httpRequest){
         this.httpRequest = httpRequest;
@@ -14,14 +21,28 @@ public class ResponseHandler {
 
     public void sendResponseDependOnRequest(DataOutputStream dos) throws IOException {
         HttpResponse httpResponse = new HttpResponse();
-        String requestMethod = httpRequest.getRequestMethod();
-        String requestPath = httpRequest.getRequestPath();
 
-        if (requestMethod.equals("GET")) {
-            httpResponse.respondHtmlFile(dos,RELATIVE_PATH + requestPath, httpRequest.getMimeType());
+        if (httpRequest.getMethod().equals("GET")) {
+            responseWhenMethodIsGet(dos,httpResponse);
+        } else if (httpRequest.getMethod().equals("POST")) {
+            if (httpRequest.getPath().startsWith("/create")) {
+                httpResponse.handleRegistrationRequest(dos, httpRequest.getRequestBody(),MAIN_PAGE_URL);
+            }
         }
-        if (requestMethod.equals("POST") && (requestPath.equals("/create"))){
-            httpResponse.handleRegistrationRequest(dos,httpRequest.getRequestBody());
+    }
+
+    public void responseWhenMethodIsGet(DataOutputStream dos, HttpResponse httpResponse) throws IOException {
+        if (httpRequest.getPath().startsWith("/user")) {
+            String sessionId = SessionManager.createSession(httpRequest.getQueryParam());
+            if (!sessionId.isEmpty()){
+                httpResponse.handleLoginRequest(dos,sessionId, MAIN_PAGE_URL);
+                logger.debug("Login Completed");
+            }else{
+                httpResponse.handleLoginRequest(dos,sessionId, LOGIN_FAIL_PAGE_URL);
+                logger.debug("Login Failed");
+            }
+        } else {
+            httpResponse.respondToHtmlFile(dos,RELATIVE_PATH + httpRequest.getPath(), httpRequest.getMimeType());
         }
     }
 }
