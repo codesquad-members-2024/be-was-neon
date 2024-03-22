be9e1e6cb533a7d64a18a5b33278b19452386820
Gyeongtaek <ogt124321@gmail.com>
2024-03-22 09:36:27 +0900

    Requst 에서 get 을 통해 정보가 들어 왔을 경우를 추가 RequestLine 을 처리할때 get 뒤에 로그인 정보가 들어있을 경우 queryParam 변수에 저장하여 사용할수 있도록 해주었습니다.

--- src/main/java/request/RequestLineParser.java
+++ src/main/java/request/RequestLineParser.java
@@ -1,11 +1,25 @@
 package request;
 
+import java.util.Map;
+
 public class RequestLineParser {
     public RequestLine parse(String requestLine){
         String[] getRequestTokens = requestLine.split(" ");
         String requestMethod =  getRequestTokens[0];
-        String requestPath = getRequestTokens[1];
+        String requestPath = parsePath(getRequestTokens[1]);
+        String queryParam = parseQueryParam(getRequestTokens[1]);
         String requestProtocol = getRequestTokens[2];
-        return new RequestLine(requestMethod,requestPath,requestProtocol);
+        return new RequestLine(requestMethod,requestPath,queryParam,requestProtocol);
+    }
+    private String parsePath(String path) {
+        if (path.contains("?")) {
+            return path.substring(0, path.indexOf("?"));
+        }
+        return path;
+    }
+    private String parseQueryParam(String path) {
+        if (path.contains("?"))
+            return path.substring(path.indexOf("?") + 1);
+        return "";
     }
 }
