863e8bd201c615c19ebf2ddf0adce30698748449
kalia <fmoon11200@naver.com>
2024-03-26 11:26:27 +0900

    리다이렉션 기능 추가 리팩토링 필요

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -4,38 +4,30 @@ import Utils.HttpRequestParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.Socket;
 
 public class HttpRequest {
     private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
-    private Socket connection;
-    private String httpRequest;
+    private final HttpRequestReader reader;
     private String path;
 
-    public HttpRequest(Socket connection) throws IOException {
-        this.connection = connection;
+    public HttpRequest(HttpRequestReader reader) throws IOException {
+        this.reader = reader;
         parseRequest();
     }
 
     private void parseRequest() throws IOException {
-        InputStream in = connection.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-
         StringBuilder requestBuilder = new StringBuilder();
         String line;
         boolean isFirstLine = true;
-        while (!(line = br.readLine()).isEmpty()) {
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
             if (isFirstLine) {
                 logger.info("요청 라인: {}", line);
                 isFirstLine = false;
             }
             requestBuilder.append(line).append("\r\n");
         }
-        httpRequest = requestBuilder.toString();
+        String httpRequest = requestBuilder.toString();
         HttpRequestParser httpRequestParser = new HttpRequestParser(httpRequest);
         path = httpRequestParser.extractPath();
     }
